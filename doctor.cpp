d6bd6c3d617351c60f087a58152f888bf531386d
SmilingYogi <38402270+SmilingYogi@users.noreply.github.com>
2018-08-30 18:16:09 +0530

    Update doctor.cpp

--- doctor.cpp
+++ doctor.cpp
@@ -1,1 +1,15 @@
 #include<iostream.h>
+#include<conio.h>
+class doctor
+{
+char docname[20];
+char docspecial[20];
+int docfees;
+};
+void main()
+{
+clrscr();
+cout<<"Make your choice
+cout<<"Register new doctor"<<endl;
+cout<<"View doctor list"<<endl;
+cout<<"Remove doctor"<<endl;
