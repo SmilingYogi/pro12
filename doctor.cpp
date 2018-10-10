452f80fd5c91cef59bfa741250bc335bc70b8d86
vijey varshan <42837798+vijeyvars@users.noreply.github.com>
2018-10-10 17:52:20 +0530

    Update doctor.cpp

--- doctor.cpp
+++ doctor.cpp
@@ -8,6 +8,7 @@ void adddoc();
 void dispdoc();
 void deldoc();
 void editdoc();
+void reset();
 class doctor
 {
 char docname[20];
@@ -41,10 +42,14 @@ void doctor::input()
  }
 void doctor::show()
 {
-	cout<<"Doctor ID==> "<<docid<<endl;
-	cout<<"Name==> "<<docname<<endl;
-	cout<<"Speciality ==> "<<docspecial<<endl;
-	cout<<"Experience==> "<<docexp<<endl;
+
+ cout<<"|"<<docid<<"|"<<"\t"<<"|"<<docname<<"|"<<"\t"<<"|"<<docspecial<<"|"<<"\t"<<"|"<<docexp<<"|"<<endl;
+
+
+       //	cout<<"Doctor ID==> "<<docid<<endl;
+	//cout<<"Name==> "<<docname<<endl;
+	//cout<<"Speciality ==> "<<docspecial<<endl;
+	//cout<<"Experience==> "<<docexp<<endl;
 }
 
 fstream fil;
@@ -61,7 +66,8 @@ cout<<"1.Register new doctor"<<endl;
 cout<<"2.View doctor list"<<endl;
 cout<<"3.Remove doctor"<<endl;
 cout<<"4.Edit doctor"<<endl;
-cout<<"5.Exit"<<endl;
+cout<<"5.System Reset"<<endl;
+cout<<"6.Exit"<<endl;
 cin>>c;
 switch(c)
  { case 1:
@@ -73,8 +79,10 @@ switch(c)
    case 4:
 	 editdoc(); break;
    case 5:
-	  exit(0); break;
-   default:
+	 reset(); break;
+   case 6:
+	 exit(0); break;
+
 	  cout<<"Invalid choice!!!";
  }}
  getch();
@@ -103,19 +111,24 @@ switch(c)
 		while(!filo.eof())
 		{       filo.read((char*)&dob, sizeof(dob));
 			if(!filo.eof())
+		       {	cout<<"Doctor ID"<<"\t"<<"Name"<<"\t"<<"Speciality"<<"\t"<<"Experience"<<endl;
 			dob.show();
-		       //	cout<<"Press Any Key....For Next Record"<<endl;
-		       //	getch();
-		       //	filo.read((char*)&dob, sizeof(dob));
-		}
+			}
+
+			}
 	}
 	filo.close();
 }
 void deldoc()		//Function to Delete Particular Record
 {
+	char g;
 	int n;
 	cout<<"Enter ID that should be Deleted :";
 	cin>>n;
+	cout<<"Are you sure about deleting the doctor ID "<<n<<"for sure ?"<<endl;
+	cin>>g;
+	if(g=='y')
+       {
 	ofstream o;
 	o.open("new.dat",ios::out|ios::binary);
 	fil.open("doc.dat",ios::in| ios::binary);
@@ -145,6 +158,8 @@ void deldoc()		//Function to Delete Particular Record
 	fil.close();
 	remove("doc.dat");
 	rename("new.dat", "doc.dat");
+	cout<<"Doctor ID "<<n<<"deleted successfully...!!!"<<endl;
+     }
 }
 void editdoc()		//Function to Modify Particular Record from Data File
 {
@@ -180,6 +195,17 @@ void editdoc()		//Function to Modify Particular Record from Data File
 	}
 	fil.close();
 }
+
+void reset()
+{
+ char ho;
+ cout<<"WARNING:Once reset,data cannot be restored."<<endl;
+ cout<<"Are you sure you want to reset?"<<endl;
+ cin>>ho;
+ if(ho='y')
+ cout<<"System reset successful....!!!!"<<endl;
+ remove("doc.dat");
+ }
 
 
 
